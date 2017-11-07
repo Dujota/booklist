43b7bb5f43d9b444c8866b0d99c13300482e5eba
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-11-06 23:40:42 -0500

    cleaned up the json display by doing a block inside the format.json to create a new hash that has books as the key and an array of hashes (one book at a time) for ease of reading

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,8 +4,21 @@ class BooksController < ApplicationController
     @books = Book.all
     respond_to do |format|
       format.html
-      format.text 
-      # format.json { render json: @resource }
+      format.text
+      format.csv do
+        render plain: Book.generate_csv(@books)
+      end
+      format.json do
+        json_data = @books.map do |book| # cleans up the way we display the data for json it was not needed to display the json data on its own
+          {
+            id: book.id,
+            title: book.title,
+            author: book.author,
+            read: book.already_read
+          }
+        end
+        render json: { books: json_data } # this will create a hash with key books and a value which is an array of the has of all books saved in json_data
+      end
     end
   end
 
